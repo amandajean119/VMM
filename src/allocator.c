763ce4ba49910674525d3ece77c56e4f79f265cd
Oscar Mondragon <oscar.mondragon@gmail.com>
2013-04-04 14:44:05 -0600

    more updates

--- src/allocator.c
+++ src/allocator.c
@@ -6,12 +6,64 @@
  */
 
 #include "allocator.h"
+#include <stdio.h>      /* printf, scanf, NULL */
+#include <stdlib.h>     /* malloc, free, rand */
+#include <time.h>       /* srand */
 
 /*
  * Compute initial population
  */
 
 int initGA(struct ga * ga){
+
+	allocConfig * config = malloc(sizeof(allocConfig));
+
+	// CPU Speeds in Khz
+	config->maxCPU = 12000000;   // Khz
+	config->minCPU = 300000;
+
+	// Percentage of utilization
+	config->maxU = 100;
+	config->minU = 80;
+
+	// Time dilation Factor
+	config->maxTdf = 20;
+	config->minTdf = 1;
+
+	// Slices and periods in micro seconds
+	config->maxSlice = 1000000; // Usec
+	config->minSlice = 1000;
+	config->maxPeriod = 100000000;
+    config->minPeriod = 100000;
+
+	ga->aConfig = config;
+	ga->populationSize = 100;
+
+	// Configuration which is constant for each machine
+	int nrPcores = 10;
+    int nrVms = 1;
+    int nrVcores = 50;
+    int i, j;
+    pcore * pcores = malloc(nrPcores*sizeof(pcore));
+    vm * vms = malloc(nrVms*sizeof(vm));
+
+    for (i = 0; i < nrPcores; i++) {
+    	srand(time(NULL));
+		pcores[i] ->speedKhz = rand() % config->maxCPU + config->minCPU;
+		pcores[i] ->maxUtilization = rand() % config->maxU + config->minU;
+
+    }
+
+    for (i = 0; i < nrVms; i++) {
+
+    	vms[i]->vcores = malloc(nrVcores*sizeof(vcore));
+
+		for (j = 0;  j < nrVcores; j++) {
+			vms[i]->vcores[j] ->speedKhz =
+					rand() % config->maxCPU + config->minCPU;
+			}
+	}
+
 
 	return 0;
 
@@ -82,7 +134,7 @@ int main(){
 	initGA(ga);
 	evaluateFitness(ga);
 
-	/*
+
 	while(nrGen < maxGen && maxFitness(ga)<100 ){
 
 		selection(ga);
@@ -91,7 +143,7 @@ int main(){
 		evaluateFitness(ga);
 
 	}
- */
+
 
 
 }
--- src/allocator.h
+++ src/allocator.h
@@ -16,80 +16,87 @@ typedef uint64_t timeUs;
  * General Data structure for the Genetic algorithm
  */
 
-struct ga {
+typedef struct ga {
 
-	struct machine * population; // Array with the different configurations
+	int populationSize;
+	machine * population; // Array with the different configurations
 								 // for a physical machine. Each element of the
 								 // array represents an individual of the
 								 // population
-    struct allocatorConfig * aConfig;	// Allocator Configuration
-};
+    allocatorConfig * aConfig;	// Allocator Configuration
+} ga;
 
 
 /*
  * Allocator configuration data structure
  */
 
-struct allocatorConfig{
+typedef struct allocatorConfig{
 
 	 timeUs minSlice;       // Minimum slice allowed for a vcore
 	 timeUs maxSlice;       // Maximum slice allowed for a vcore
 	 timeUs minPeriod;      // Minimum period allowed for a vcore
 	 timeUs maxPeriod;      // Maximum period allowed for a vcore
-	 int minTdf;			 // Minimum time dilation factor allowed for a vm
-	 int maxTdf;		     // Maximum time dilation factor allowed for a vm
+	 int minTdf;			// Minimum time dilation factor allowed for a vm
+	 int maxTdf;		    // Maximum time dilation factor allowed for a vm
+     int minCPU;            // Minimum CPU speed
+     int maxCPU;   			// Maximum CPU speed
+     int minU;				// Minimum utilization factor
+     int maxU;				// Maximum utilization factor
 
-
-};
+} allocConfig;
 
 
 /*
  * Physical Machine data structure
  */
 
-struct machine {
+typedef struct machine {
 
+	int nrPcores;
+	int nrVms;
 	struct pcore * pcores;  // Array of physical cores in this physical machine
     struct vm * vms;		// Pointer to the arrray of vms of this machine
-    int fitness;			// Fitness function for this physical machine
+
+    float fitness;			// Fitness function for this physical machine
     						// configuration
-};
+} machine;
 
 
 /*
  * Virtual Machine data structure
  */
 
-struct vm {
+typedef struct vm {
 
 	int nrVcores;			// Number of virtual cores in this virtual machine
 	int tdf;                // Time dilation factor for this vm
 	struct vcore * vcores;  // Pointer to the array of virtual cores of this
 							// vm
-};
+} vm;
 
 /*
  * Virtual core data structure
  */
 
-struct vcore{
+typedef struct vcore{
 
 	timeUs slice;			// Size of the slice in usec
 	timeUs period;			// Size of the period in usec
 	int speedKhz;			// vcpu speed in Khz
 	struct phCore * pcore;  // Physical core allocated to this vcore
-};
+} vcore;
 
 /*
  * Physical core data structure
  */
 
-struct pcore{
+typedef struct pcore{
 
 	int speedKhz;			// pcpu speed in Khz
     int maxUtilization;		// Maximum utilization (Range 0 - 100)
     int utilization;		// Achieved utilization
-};
+} pcore;
 
 
 
